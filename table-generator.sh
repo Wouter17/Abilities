627a5e6cf26485766f897f9aae1e646af6c7194f
Wouter Breedveld <W.J.Breedveld-1@student.tudelft.nl>
2022-08-30 14:48:45 +0200

    Added manual

---
+++ table-generator.sh
@@ -0,0 +1,31 @@
+location=./Manual/HTML/img/Abilities
+declare -i entriesPerPage=13
+declare -i page=2
+declare -i counter=0
+
+for entry in "$location"/*;
+do
+	echo "<tr>"
+	echo "    <td><img class='ability-icon' src='./img/Abilities/$(basename "$entry")'/></td>"
+	v=$(basename "$entry")
+	echo "    <td>${v%.*}</td>"
+	echo "</tr>"
+	if [ "$counter" -ge "$entriesPerPage" ];
+	then
+		counter=0
+		printf "</table></div><div class=\"page-footer relative-footer\">%d</div></div>" $page
+		((page++))
+        printf "<div class=\"page page-bg-%02d\">" $page
+        echo '<div class="page-header">'
+        echo '<span class="page-header-doc-title">Keep Talking and Nobody Explodes Mod</span>'
+        echo '<span class="page-header-section-title">A: Abilities</span>'
+        echo '</div><div class="page-content"><table class="abilities-table">'
+	else
+		((counter++))
+	fi
+done
+echo "</table>"
+echo ""
+echo "Pages: $page" 
+		
+$SHELL
